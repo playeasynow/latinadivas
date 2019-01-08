6c25af3d394dfc187741d685ca8df1bbc53dd6a7
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-08 15:26:30 -0600

    Updates

---
+++ precache-manifest.50336586566c31f177f02315a948a83e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc76fad72c1a626912ce",
+    "url": "/LatinaDivas/static/js/runtime~main.fc76fad7.js"
+  },
+  {
+    "revision": "a823b51461e2a03a1833",
+    "url": "/LatinaDivas/static/js/main.a823b514.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/LatinaDivas/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "a823b51461e2a03a1833",
+    "url": "/LatinaDivas/static/css/main.09233dae.chunk.css"
+  },
+  {
+    "revision": "3cbe6335e86cb0c0515df4bdd433156a",
+    "url": "/LatinaDivas/index.html"
+  }
+];
