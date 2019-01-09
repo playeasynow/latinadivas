6800f74b6741b33851431f7a8e3c9bcd7703b98e
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-09 14:20:03 -0600

    added screenshot for readme

--- latinx-app/src/components/Header/Header.js
+++ latinx-app/src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = props => (
   <div className="header">
     <div className="title">{props.children}</div>
     <div className="scores">
-      Current Score: {props.score} Highest Score: {props.highscore}
+      Current Score: <span className="digit">{props.score}</span> Highest Score: <span className="digit">{props.highscore}</span>
     </div>
   </div>
 );
