e98817c268e45664dbc5f5d8b164479dea544eb2
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-02-07 11:29:26 -0600

    Updates

---
+++ precache-manifest.d9e86740568cf13f8391ef680ac27dee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8066cd790eac38c40e1",
+    "url": "/LatinaDivas/static/css/main.7e00739f.chunk.css"
+  },
+  {
+    "revision": "d8066cd790eac38c40e1",
+    "url": "/LatinaDivas/static/js/main.d8066cd7.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/LatinaDivas/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "fc76fad72c1a626912ce",
+    "url": "/LatinaDivas/static/js/runtime~main.fc76fad7.js"
+  },
+  {
+    "revision": "998bb86e85d49ae09c90d4a10d50f4e2",
+    "url": "/LatinaDivas/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/LatinaDivas/precache-manifest.a9a8cf08f75b7295636f35c900927dd4.js"
+  "/LatinaDivas/precache-manifest.d9e86740568cf13f8391ef680ac27dee.js"
 );
 
 workbox.clientsClaim();
